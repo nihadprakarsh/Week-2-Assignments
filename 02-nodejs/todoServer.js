a88dffce0f1ba07efbfb2d995bc2796b652ba58f
Nihad Prakarsh <nihadprakarsh@gmail.com>
2023-07-02 20:44:55 +0530

    Hi this is assignment

--- 02-nodejs/todoServer.js
+++ 02-nodejs/todoServer.js
@@ -44,6 +44,72 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+
+function findIndex(arr, id) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].id === id) return i;
+  }
+  return -1;
+}
+
+
+function removeAtIndex(arr, index) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i !== index) newArray.push(arr[i]);
+  }
+  return newArray;
+}
+
+
 app.use(bodyParser.json());
+let todos = [];
+
+app.get("/todos",(req,res)=>{
+  res.json(todos);
+})
+
+app.post("/todos",(req,res)=>{
+  let newTodo = {
+      title:req.body.title,
+      id: Math.floor(Math.random()*100000),
+      description: req.body.description,
+  };
+
+  todos.push(newTodo);
+  res.status(200).json(newTodo);
+});
+
+app.get('/todos/:id', (req, res) => {
+  const todoIndex = findIndex(todos, parseInt(req.params.id));
+  if (todoIndex === -1) {
+    res.status(404).send();
+  } else {
+    res.json(todos[todoIndex]);
+  }
+});
+
+app.delete('/todos/:id', (req, res) => {
+  const todoIndex = findIndex(todos, parseInt(req.params.id));
+  if (todoIndex === -1) {
+    res.status(404).send();
+  } else {
+    todos = removeAtIndex(todos, todoIndex);
+    res.status(200).send();
+  }
+});
+
+app.put("/todos/:id",(req,res)=>{
+  let ind = findIndex(todos, parseInt(req.params.id));
+  todos[ind].title = req.body.title;
+  todos[ind].description = req.body.description;
+  res.status(200).json(todos[ind]);
+});
+
 
 module.exports = app;
+app.use((req, res, next) => {
+  res.status(404).send();
+});
+
+app.listen(3000);
